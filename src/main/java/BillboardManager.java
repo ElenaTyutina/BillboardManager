8e50da62364e142b49231a115884ced49a388679
Elena Tuytina <yptyutina@mail.ru>
2022-01-24 22:54:29 +0300

    Manager1

--- src/main/java/BillboardManager.java
+++ src/main/java/BillboardManager.java
@@ -1,34 +1,16 @@
 public class BillboardManager {
 
-    private Poster[] posters ;
-    private int postersCount=0;
-
-//    public void addPoster(Poster poster) {
-//        int length = posters.length + 1;
-//
-//        Poster[] tmp = new Poster[length];
-//        for (int i = 0; i < posters.length; i++) {
-//            tmp[i] = posters[i];
-//
-//            int lastIndex = posters.length - 1;
-//            tmp[lastIndex] = poster;
-//            posters = tmp;
-//        }
-//    }
+    private Poster[] posters;
+    private int postersCount = 0;
 
     public void addPoster(Poster poster) {
-        if ( postersCount < posters.length )
-            postersCount = postersCount+1;
-        for (int i = postersCount  - 1; i > 0; i--) {
-            posters[i] = posters [i - 1];
+        if (postersCount < posters.length)
+            postersCount = postersCount + 1;
+        for (int i = postersCount - 1; i > 0; i--) {
+            posters[i] = posters[i - 1];
         }
         posters[0] = poster;
-
-
-
     }
-
-
 
     public BillboardManager() {
         this(10);
@@ -47,20 +29,6 @@ public class BillboardManager {
         return tmp;
 
     }
-
-//    public Poster[] recentMovies() {
-//
-//        int resultLength;
-//        if (posters.length < posterBatchSize) {
-//            resultLength = posters.length;
-//        } else {
-//            resultLength = posterBatchSize;
-//        }
-//        Poster[] tmp = new Poster[resultLength];
-//        for (int i = posters.length - resultLength; i < posters.length; i++);
-//        return tmp;
-//    }
-
 }
 
 
--- src/test/java/BillboardManagerTest.java
+++ src/test/java/BillboardManagerTest.java
@@ -1,27 +1,28 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.Date;
 
 public class BillboardManagerTest {
 
     @Test
     public void add3() {
-    BillboardManager manager= new BillboardManager();
+        BillboardManager manager = new BillboardManager();
         Poster poster1 = new Poster("comedy", "film 1", new Date());
         manager.addPoster(poster1);
         Poster poster2 = new Poster("comedy", "film 2", new Date());
         manager.addPoster(poster2);
 
         Poster[] actual = manager.getPosters();
-        Poster[] expected = new Poster[] {poster2,poster1};
+        Poster[] expected = new Poster[]{poster2, poster1};
 
-        assertArrayEquals(expected,actual);
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     public void add4() {
-        BillboardManager manager= new BillboardManager(2);
+        BillboardManager manager = new BillboardManager(2);
         Poster poster1 = new Poster("comedy", "film 1", new Date());
         manager.addPoster(poster1);
         Poster poster2 = new Poster("comedy", "film 2", new Date());
@@ -30,8 +31,8 @@ public class BillboardManagerTest {
         manager.addPoster(poster3);
 
         Poster[] actual = manager.getPosters();
-        Poster[] expected = new Poster[] {poster3,poster2};
+        Poster[] expected = new Poster[]{poster3, poster2};
 
-        assertArrayEquals(expected,actual);
+        assertArrayEquals(expected, actual);
     }
 }
