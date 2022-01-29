993ba202deb03875a72c2bfb6f726b1280f21cb4
Elena Tuytina <yptyutina@mail.ru>
2022-01-29 11:40:11 +0300

    Manager3

--- src/test/java/BillboardManagerTest.java
+++ src/test/java/BillboardManagerTest.java
@@ -7,7 +7,7 @@ import java.util.Date;
 public class BillboardManagerTest {
 
     @Test
-    public void add3() {
+    public void testLess() {
         BillboardManager manager = new BillboardManager();
         Poster poster1 = new Poster("comedy", "film 1", new Date());
         manager.addPoster(poster1);
@@ -21,7 +21,7 @@ public class BillboardManagerTest {
     }
 
     @Test
-    public void add4() {
+    public void testMore() {
         BillboardManager manager = new BillboardManager(2);
         Poster poster1 = new Poster("comedy", "film 1", new Date());
         manager.addPoster(poster1);
@@ -35,4 +35,40 @@ public class BillboardManagerTest {
 
         assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void Equals() {
+        BillboardManager manager = new BillboardManager();
+        Poster poster1 = new Poster("comedy", "film 1", new Date());
+        manager.addPoster(poster1);
+        Poster poster2 = new Poster("comedy", "film 2", new Date());
+        manager.addPoster(poster2);
+        Poster poster3 = new Poster("comedy", "film 2", new Date());
+        manager.addPoster(poster3);
+        Poster poster4 = new Poster("comedy", "film 2", new Date());
+        manager.addPoster(poster4);
+        Poster poster5 = new Poster("comedy", "film 2", new Date());
+        manager.addPoster(poster5);
+        Poster poster6 = new Poster("comedy", "film 2", new Date());
+        manager.addPoster(poster6);
+        Poster poster7 = new Poster("comedy", "film 2", new Date());
+        manager.addPoster(poster7);
+        Poster poster8 = new Poster("comedy", "film 2", new Date());
+        manager.addPoster(poster8);
+        Poster poster9 = new Poster("comedy", "film 2", new Date());
+        manager.addPoster(poster9);
+        Poster poster10 = new Poster("comedy", "film 2", new Date());
+        manager.addPoster(poster10);
+
+        Poster[] actual = manager.getPosters();
+        Poster[] expected = new Poster[]{poster10, poster9, poster8, poster7, poster6, poster5, poster4, poster3, poster2, poster1};
+
+        assertArrayEquals(expected, actual);
+    }
+
+
 }
+
+
+
+
